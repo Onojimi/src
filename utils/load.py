4a166bd81be82f6778e541c0746dae3b8fd85136
Onojimi <18811505578@163.com>
2019-08-07 14:40:54 -0400

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -10,11 +10,11 @@ from utils.utils import resize_and_crop, get_square, normalize, hwc_to_chw
 
 
 def get_ids(img_dir):
-    '''读取当前文件夹下的所有文件名'''
+#       读取当前文件夹下的所有文件名
     return (f[:-4] for f in os.listdir(img_dir))
 
 def split_ids(ids, n=2):
-    '''构建元组（文件名，0/1）'''
+#         构建元组（文件名，0/1）
     return((id, i) for id in ids for i in range(n))
 
 def to_cropped_imgs(ids, img_dir, suffix, scale):
@@ -28,7 +28,7 @@ def get_imgs_and_masks(ids, img_dir, mask_dir, scale):
     imgs_switched = map(hwc_to_chw, imgs)
     imgs_normalized = map(normalize, imgs_switched)
     
-    masks = to_cropped_imgs(ids, mask_dir, "_masks.png", scale)
+    masks = to_cropped_imgs(ids, mask_dir, "_mask.png", scale)
     return zip(imgs_normalized, masks)
 
 def get_full_img_and_mask(id, img_dir, mask_dir):
