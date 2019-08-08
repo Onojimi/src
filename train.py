786effbb85047a721482087f6f950fc8fc9ecea9
Onojimi <18811505578@163.com>
2019-08-08 10:55:31 -0400

    Update train.py

--- train.py
+++ train.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
         
     if args.gpu:
         pdb.set_trace()
-        if torch.device_count()>1:
+        if torch.cuda.device_count()>1:
             net = nn.DataParallel(net)
         net.cuda()
         
