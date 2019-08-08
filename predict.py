63aae2a0043f7ecdd24cbdfb45a3b59a102db052
Onojimi <18811505578@163.com>
2019-08-08 16:59:19 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,15 +1,10 @@
-import sys
 import os
+import argparse
 import numpy as np
-from optparse import OptionParser
 from PIL import Image
 
 import torch
-import torch.backends.cudnn as cudnn
-import torch.nn as nn
-from torch import optim
 
-from eval import eval_net
 from unet.unet_model import Unet
 from utils.load import get_ids, split_ids, get_imgs_and_masks
 from utils.utils import split_train_val, batch, normalize, resize_and_crop,\
@@ -86,5 +81,67 @@ def get_args():
 
     return parser.parse_args()     
 
-   
+def get_output_filenames(args):
+    in_files = args.input
+    out_files = []
+
+    if not args.output:
+        for f in in_files:
+            pathsplit = os.path.splitext(f)
+            out_files.append("{}_OUT{}".format(pathsplit[0], pathsplit[1]))
+    elif len(in_files) != len(args.output):
+        print("Error : Input files and output files are not of the same length")
+        raise SystemExit()
+    else:
+        out_files = args.output
+
+    return out_files   
+
+def mask_to_image(mask):
+    return Image.fromarray((mask * 255).astype(np.uint8))
+
+if __name__ == "__main__":
+    args = get_args()
+    in_files = args.input
+    out_files = get_output_filenames(args)
+
+    net = Unet(n_channels=3, n_classes=1)
+
+    print("Loading model {}".format(args.model))
+
+    if not args.cpu:
+        print("Using CUDA version of the net, prepare your GPU !")
+        net.cuda()
+        net.load_state_dict(torch.load(args.model))
+    else:
+        net.cpu()
+        net.load_state_dict(torch.load(args.model, map_location='cpu'))
+        print("Using CPU version of the net, this may be very slow")
+
+    print("Model loaded !")
+    
+    for i, fn in enumerate(in_files):
+        print("\nPredicting image {} ...".format(fn))
+
+        img = Image.open(fn)
+        if img.size[0] < img.size[1]:
+            print("Error: image height larger than the width")
+
+        mask = predict_img(net=net,
+                           full_img=img,
+                           scale_factor=args.scale,
+                           out_threshold=args.mask_threshold,
+                           use_dense_crf= not args.no_crf,
+                           use_gpu=not args.cpu)
+
+#         if args.viz:
+#             print("Visualizing results for image {}, close to continue ...".format(fn))
+#             plot_img_and_mask(img, mask)
+
+        if not args.no_save:
+            out_fn = out_files[i]
+            result = mask_to_image(mask)
+            result.save(out_files[i])
+
+            print("Mask saved to {}".format(out_files[i]))
     
