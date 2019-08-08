c9a5ced0646029155dd39f77bf394bbc12c3f6f4
Onojimi <18811505578@163.com>
2019-08-07 21:30:00 -0400

    Update train.py

--- train.py
+++ train.py
@@ -83,12 +83,13 @@ def train_net(net,
                 
                 masks_pred_detach = masks_pred.cpu().detach().numpy()
                 masks_pred_show = np.transpose(masks_pred_detach, [0,2,3,1])
+                masks_pred_show = masks_pred_show*255
                 
                 print(masks_pred_show[0])
-#                 mask_show1 = Image.fromarray(masks_pred_show[0], 'RGB')
-#                 mask_show2 = Image.fromarray(masks_pred_show[1], 'RGB')
-#                 
-#                 mask_show1.show()
+                mask_show1 = Image.fromarray(masks_pred_show[0], 'L')
+                mask_show2 = Image.fromarray(masks_pred_show[1], 'L')
+                 
+                mask_show1.show()
                 
             
                 masks_probs_flat = masks_pred.view(-1)
