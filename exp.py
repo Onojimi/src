ef337b4d443bd2135f21c30e82ce27992c0b0713
Onojimi <18811505578@163.com>
2019-08-09 13:15:30 -0400

    Update exp.py

--- exp.py
+++ exp.py
@@ -3,12 +3,14 @@ import torch
 import numpy as np
 from PIL import Image
 
-tensor1 = torch.tensor([[[2,4,1],[2,4,1]]])
-tensor2 = torch.tensor([[[2,1,1],[2,1,1]]]) 
-print(tensor1.shape,tensor2.shape) 
-print(tensor1[0].shape)
-tensor1 = tensor1.squeeze()  
-print(tensor1.shape,tensor2.shape) 
+# tensor1 = torch.tensor([[[2,4,1],[2,4,1]]])
+# tensor2 = torch.tensor([[[2,1,1],[2,1,1]]]) 
+# print(tensor1.shape,tensor2.shape) 
+# print(tensor1[0].shape)
+# tensor1 = tensor1.squeeze()  
+# print(tensor1.shape,tensor2.shape) 
+# li= {'season' : ['a','b','c','d'], 'numbers' : [1,2,3,4]}
+# print(li['season'])
 
 # img = Image.open("pig.png")
 # print(img.size)
@@ -22,12 +24,12 @@ print(tensor1.shape,tensor2.shape)
 
 # print(tensor1.view(-1),tensor2.view(-1))
 # print(torch.dot(tensor1.view(-1),tensor2.view(-1)))
-# img = Image.open("big-pig-face.png")
-# print(img.size)
-# img1 = img.resize([300,300])
-# img1.save("pig.png")
-# img_np= np.array(img)
-# print(img_np.shape)
+img = Image.open("car.jpg")
+print(img.size)
+img1 = img.resize([800,800])
+img1.save("car.tif")
+img_np= np.array(img)
+print(img_np.shape)
 
 # 
 # img_crop = img.crop((0, 30, img.size[0], img.size[1]-30))
