77ad3f9c998f2db792a4fc3c0f5678a801dc7847
Onojimi <18811505578@163.com>
2019-08-12 10:29:58 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,7 +1,17 @@
 import torch
 import torch.nn.functional as F
+import numpy as np
+import pdb
 
 from dice_loss import dice_coeff
+
+def compute_iou(true, pred):
+    true_mask = np.asanyarray(true.convert('L'), dtype = np.bool)
+    pred_mask = np.asanyarray(pred.convert('L'), dtype = np.bool)
+    union = np.sum(np.logical_or(true_mask, pred_mask))
+    intersection = np.sum(np.logical_and(true_mask, pred_mask))
+    iou = intersection/union
+    return iou
 
 def eval_net(net, dataset, gpu = False):
     net.eval()
@@ -15,11 +25,13 @@ def eval_net(net, dataset, gpu = False):
         
         if gpu:
             img = img.cuda()
-            true_mask = true_mask.cuda()
+#            true_mask = true_mask.cuda()
         
         mask_pred = net(img)[0]
+        print(type(mask_pred))
+        pdb.set_trace()
         mask_pred = (mask_pred > 0.5).float()
         
-        tot+=dice_coeff(input = mask_pred, target = true_mask).item()
+#        tot+=dice_coeff(input = mask_pred, target = true_mask).item()
     
     return tot/(i+1)
