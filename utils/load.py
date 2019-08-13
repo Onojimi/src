fb1b695687357880b597c51a33091fc6dd1708e6
Onojimi <18811505578@163.com>
2019-08-13 08:19:35 -0400

    Update load.py

--- utils/load.py
+++ utils/load.py
@@ -14,7 +14,8 @@ def get_ids(img_dir):
 
 def to_cropped_imgs(ids, img_dir, suffix, scale):
     for id in ids:
-        img = np.array(Image.open(img_dir + id + suffix), dtype = np.float32)
+#        img = np.array(Image.open(img_dir + id + suffix), dtype = np.float32)
+        img = np.array(Image.open(img_dir + id + suffix))
         yield img
         
 def get_imgs_and_masks(ids, img_dir, mask_dir, scale):
